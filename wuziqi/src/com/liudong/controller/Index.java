e7489e542b5f78b82194056c464f681791c323cb
liudong <liudong@liudong-PC>
2017-03-05 14:53:52 +0800

    f

--- wuziqi/src/com/liudong/controller/Index.java
+++ wuziqi/src/com/liudong/controller/Index.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class Index {
-	@RequestMapping("/index")
+	@RequestMapping("index")
 	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("wuziqi/*.do")
+	public String index2() {
 		return "index";
 	}
 }
