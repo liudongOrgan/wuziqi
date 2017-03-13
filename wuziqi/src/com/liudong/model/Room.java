7304e4cad2bf5a14ed07e2a30b09c9a986c1ef26
WIN-UKVER4QS578\liudong-group <758657013@qq.com>
2017-03-13 18:56:30 +0800

    增加注释

--- wuziqi/src/com/liudong/model/Room.java
+++ wuziqi/src/com/liudong/model/Room.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Room {
 	private Integer status = 0;// 0默认 待加入 1 等待开始 2 进行中 3结束
-	private String user1Name;
-	private String user2Name;
-	private String roomeName;
-	private Date createDate;
+	private String user1Name;// 黑棋用户
+	private String user2Name;// 白棋用户
+	private String roomeName;// 房间名
+	private Date createDate;// 创建时间
 	private volatile Chessboard chessBoard = new Chessboard(this);
 	private volatile boolean user1Ready = false;
 	private volatile boolean user2Ready = false;
@@ -17,6 +17,9 @@ public class Room {
 		this.status = status;
 	}
 
+	/**
+	 * 黑棋用户
+	 */
 	public String getUser1Name() {
 		return user1Name;
 	}
@@ -25,6 +28,9 @@ public class Room {
 		this.user1Name = user1Name;
 	}
 
+	/**
+	 * 白棋用户
+	 */
 	public String getUser2Name() {
 		return user2Name;
 	}
@@ -33,6 +39,9 @@ public class Room {
 		this.user2Name = user2Name;
 	}
 
+	/**
+	 * 房间名称
+	 */
 	public String getRoomeName() {
 		return roomeName;
 	}
@@ -41,6 +50,9 @@ public class Room {
 		this.roomeName = roomeName;
 	}
 
+	/**
+	 * 房间创建时间
+	 */
 	public Date getCreateDate() {
 		return createDate;
 	}
@@ -49,6 +61,9 @@ public class Room {
 		this.createDate = createDate;
 	}
 
+	/**
+	 * 棋盘
+	 */
 	public Chessboard getChessBoard() {
 		return chessBoard;
 	}
@@ -69,28 +84,46 @@ public class Room {
 		this.user2Ready = user2Ready;
 	}
 
+	/**
+	 * 当前房间状态
+	 */
 	public Integer getStatus() {
 		return status;
 	}
 
+	/**
+	 * 获得当前下子用户名
+	 */
 	public String getNextChessUserName() {
 		return chessBoard.getNextChessUserName();
 	}
 
+	/**
+	 * 落子
+	 */
 	public boolean chess(Chess chess) {
 		if (2 != this.status.intValue())
 			return false;
 		return chessBoard.chess(chess);
 	}
 
+	/**
+	 * 判断是否有人获胜
+	 */
 	public Color checkWin() {
 		return chessBoard.checkWin();
 	}
 
+	/**
+	 * 是否游戏结束,有人获胜及结束
+	 */
 	public boolean isOver() {
 		return chessBoard.isOver();
 	}
 
+	/**
+	 * 获得落子列表
+	 */
 	public List<Chess> getChessedList() {
 		return chessBoard.getChessedList();
 	}
